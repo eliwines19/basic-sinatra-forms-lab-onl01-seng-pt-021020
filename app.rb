15fa91809f34ea59d12f082d9285be3e71ba95e9
Elijah wines <elijahwines84@yahoo.com>
2020-05-05 18:30:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,19 +3,19 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
   get '/newteam' do
-      erb :newteam
+    erb :newteam
   end
 
   post '/team' do
-      @name = params[:name]
-      @coach = params[:coach]
-      @pg = params[:pg]
-      @sg = params[:sg]
-      @pf = params[:pf]
-      @sf = params[:sf]
-      @c = params[:c]
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
 
-      erb :team
+    erb :team
   end
 
 end
